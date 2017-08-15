d106ae42b107351741e5cef7b7ecd6bebe9b7db3
wkwksama <mailpakgasing@gmail.com>
2017-08-15 21:06:02 +0700

    starbase: introducing api

--- starbase/src/App.vue
+++ starbase/src/App.vue
@@ -1,6 +1,7 @@
 <template>
 <div id="app">
   <h3>{{ title }}</h3>
+  <button class="btn btn-primary" @click="fetchCharacter">coba fetch char</button>
 </div>
 </template>
 
@@ -9,7 +10,16 @@ export default {
   name: 'app',
   data() {
     return {
-      title: 'Generate your team'
+      title: 'Generate your team asd'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people/1', {
+        method: 'GET'
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
     }
   }
 }
