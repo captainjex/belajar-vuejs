a46b152b600abd95b258a6f4a4c5dd1fcf52cc96
wkwksama <mailpakgasing@gmail.com>
2017-08-16 16:06:45 +0700

    starbase-router: router-linking different pages

--- starbase-router/src/App.vue
+++ starbase-router/src/App.vue
@@ -1,6 +1,10 @@
 <template>
  <div id="app">
    <h3>Starbase Router hehehe</h3>
+   <h4>
+      <router-link class="link" to="/data/people">Meet the Character</router-link> |
+      <router-link class="link" to="/data/planets">Explore the Planet</router-link>
+   </h4>
    <router-view></router-view>
  </div>
 </template>
